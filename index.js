4dda7df21198f32997394fa37d1e595d412b6d63
Peter G.A. Barraud <barraud@adobe.com>
2017-11-11 13:20:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+require('./src/styles/main.css');
+
+require("domready");
+require("mini.css");
+
+// require("dead-simple-grid-npm");
+// require("flexboxgrid");
+// require("normalize.css");
+// require("pui-css-buttons");
+// require("purecss");
+
+require('./src/js/main');
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+// DOM is ready
+require('domready')(function () {
+    const sayhello = document.getElementById("sayhello");
+    const showhello = document.getElementById("showhello");
+
+    sayhello.addEventListener("click", function(){
+      showhello.innerText = "Allo, allo, gain";
+    });
+});
