a9288afdadb91c1f3c6bcd67b815bff07c73a1a3
Peter G.A. Barraud <barraud@adobe.com>
2017-11-15 09:06:00 +0530

    commented out jquery stuff. npm i jquery and uncomment to use jquery

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,16 @@
-// DOM is ready
-// require('domready')(function () {
-//     const sayhello = document.getElementById("sayhello");
-//     const showhello = document.getElementById("showhello");
+// DOM is ready with plain-old vanilla JS
+require('domready')(function () {
+    const sayhello = document.getElementById("sayhello");
+    const showhello = document.getElementById("showhello");
 
-//     sayhello.addEventListener("click", function(){
-//       showhello.innerText = "Allo, allo";
-//     });
-// });
+    sayhello.addEventListener("click", function(){
+      showhello.innerText = "Allo, allo";
+    });
+});
 
 // and if you want to use jquery instead of plain-old vanilla JS
-$(function(){
-  $( "#sayhello" ).click(function() {
-    $('#showhello').text('Allo, allo');
-  });   
-});
+// $(function(){
+//   $( "#sayhello" ).click(function() {
+//     $('#showhello').text('Allo, allo');
+//   });   
+// });
